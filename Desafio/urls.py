faee79e253c540992fc97280bcffd45c95a7a9a3
remusezequiel <ezequielremus@gmail.com>
2020-02-01 12:25:46 -0300

    Agrego configuracion para heroku

--- Desafio/urls.py
+++ Desafio/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('box/',include('bigbox.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
