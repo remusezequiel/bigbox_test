f358f3ed738142ba13c0de53b50f3447bbf4082b
remusezequiel <ezequielremus@gmail.com>
2020-02-01 15:38:20 -0300

    Configuro wsgi

--- Desafio/wsgi.py
+++ Desafio/wsgi.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 from whitenoise.django import DjangoWhiteNoise
+
 application = DjangoWhiteNoise(application)
 
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Desafio.settings')
-
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
