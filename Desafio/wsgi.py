f212fd9f232c90dde287e474876a6074b583a887
remusezequiel <ezequielremus@gmail.com>
2020-02-01 19:14:44 -0300

    jajoje

--- Desafio/wsgi.py
+++ Desafio/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
+"""
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Desafio.settings")
 
@@ -16,3 +16,19 @@ from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+"""
+
+"""
+WSGI config for djangoBlog project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBlog.settings')
+
+application = get_wsgi_application()
