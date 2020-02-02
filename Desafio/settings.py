2f98919485164d90c0213fefb0d3ee38acbdec30
remusezequiel <ezequielremus@gmail.com>
2020-02-02 15:33:27 -0300

    ..

--- Desafio/settings.py
+++ Desafio/settings.py
@@ -76,26 +76,6 @@ WSGI_APPLICATION = 'Desafio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'bigbox',
-        'USER' : 'postgres',
-        'PASSWORD' : '197382465e',
-        'HOST' : '127.0.0.1',
-        'DATABASE_PORT' : '5432',
-    }
-}
-"""
 
 import dj_database_url
 from decouple import config
@@ -106,6 +86,26 @@ DATABASES = {
     )
 }
 
+"""
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+}
+"""
+"""
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME' : 'bigbox',
+'USER' : 'postgres',
+'PASSWORD' : '197382465e',
+'HOST' : '127.0.0.1',
+'DATABASE_PORT' : '5432',
+}
+}
+"""
 #db_from_env = dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(db_from_env)
 
