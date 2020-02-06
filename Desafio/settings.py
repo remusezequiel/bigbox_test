bf5839e3a5bce0b4bc498905c835cdca5a78c309
remusezequiel <ezequielremus@gmail.com>
2020-02-06 09:38:24 -0300

    Realizo limpieza de codigo comentado

--- Desafio/settings.py
+++ Desafio/settings.py
@@ -80,14 +80,6 @@ WSGI_APPLICATION = 'Desafio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -99,28 +91,6 @@ DATABASES = {
     }
 }
 
-
-
-"""
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-}
-}
-"""
-"""
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.postgresql_psycopg2',
-'NAME' : 'bigbox',
-'USER' : 'postgres',
-'PASSWORD' : '197382465e',
-'HOST' : '127.0.0.1',
-'DATABASE_PORT' : '5432',
-}
-}
-"""
 #db_from_env = dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(db_from_env)
 
--- bigbox/views.py
+++ bigbox/views.py
@@ -10,33 +10,21 @@ from .models import Activity,Box,Category,CommonInfo
 # Create your views here.
 
 ################################################################################
-class BoxObjectMixin(object):
-    model = Box
-    lookup = 'id'
-
-    def get_object(self):
-        lookup = self.kwargs.get(self.lookup)
-        return get_object_or_404(Box, id=lookup)
-################################################################################
-
-################################################################################
 class BoxListView(ListView):
     model = Box
     template_name = 'bigbox/box.html' #<app>/<model>_<viewtype>.html
-    #Esto es lo que va a aparecer en nuestro template
     context_object_name = 'boxes'
-    #Para que aparescan del mas nuevo al mas viejo
-    #ordering = ['internal_name']
-
 ################################################################################
 
 ################################################################################
-class BoxDetailView(DetailView, BoxObjectMixin):
+class BoxDetailView(DetailView):
     model = Box
     template_name = 'bigbox/box_detail.html'
 
     def get_object(self):
-        #self.kwargs se utiliza para acceder a parametros en class based views
+        """
+        Tomo el id del Box correspondiente al del detalle
+        """
         id = self.kwargs.get("id")
         return get_object_or_404(Box, id=id)
 ################################################################################
@@ -45,8 +33,6 @@ class BoxDetailView(DetailView, BoxObjectMixin):
 class ActivityListView(DetailView):
     model = Box
     template_name = 'bigbox/activity_list.html'
-    #ordering = ['name']
-    #paginate_by = 20
 
     def get_context_data(self, **kwargs):
         """
@@ -67,19 +53,6 @@ class ActivityListView(DetailView):
         #self.kwargs se utiliza para acceder a parametros en class based views
         id = self.kwargs.get("id")
         return get_object_or_404(Box, id=id)
-
-"""
-class ActivityListView(ListView, BoxObjectMixin):
-    model = Activity
-    template_name = 'bigbox/activity_list.html'
-    #ordering = ['name']
-    paginate_by = 20
-
-    def get_context_data(self,*args,**kwargs):
-        context=super().get_context_data(*args,**kwargs)
-        context['lola'] = self.get_object()
-        return context
-"""
 ################################################################################
 
 ################################################################################
